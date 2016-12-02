f195f2ee348ad979da75f46d74364b2d216c5e58
dawang <314374355@qq.com>
2016-12-02 15:39:18 +0800

    game update

--- public/js/game.js
+++ public/js/game.js
@@ -1,43 +1,57 @@
-(function($, cc){
+(function($){
+    var socket = io('/game');
     var roomData = {
         onlineCount: 0
     };
-    var socket = io('http://127.0.0.1:8080/game');
-    console.log('加载');
-    socket.emit('login', CONFIG.user, function(res){
-        console.log('登录');
+    var ltRoomData = {
+        message: [],
+        content: ''
+    };
+    var playersData = {
+        onlineUsers: {}
+    };
+    socket.on('connect', function(){
+        socket.emit('login', CONFIG.user, function(res){
+            console.log('登录');
+        });
+        socket.on('update', function(res) {
+            console.log('更新');
+            roomData.onlineCount = res.onlineCount;
+            playersData.onlineUsers = res.onlineUsers;
+            console.log(res);
+        });
+        socket.on('message', function(res){
+            console.log(res);
+            ltRoomApp.addTodo(res);
+        });
     });
-    socket.on('update', function(res) {
-        console.log('更新');
-        roomData.onlineCount = res.onlineCount;
-        console.log(roomData);
-    });
+
     var roomDataApp = new Vue({
         el: '#room-data',
         data: roomData
     });
 
+    var ltRoomApp = new Vue({
+        el: '#lt',
+        data: ltRoomData,
+        methods: {
+            send: function(){
+                var app = this;
+                socket.emit('message', {message: app.content}, function(res){
+                    if(res.status === 200){
+                        app.content = '';
+                    }
+                });
+            },
+            addTodo: function(res){
+                this.message.push(res);
+            }
+        }
+    });
 
-    cc.game.onStart = function(){
-        //load resources
-        cc.LoaderScene.preload(["/cocos2d/HelloWorld.png"], function () {
-            var MyScene = cc.Scene.extend({
-                onEnter:function () {
-                    this._super();
-                    var size = cc.director.getWinSize();
-                    var sprite = cc.Sprite.create("/cocos2d/HelloWorld.png");
-                    sprite.setPosition(size.width / 2, size.height / 2);
-                    sprite.setScale(0.8);
-                    this.addChild(sprite, 0);
+    var playersApp = new Vue({
+        el: '#players',
+        data: playersData
+    });
 
-                    var label = cc.LabelTTF.create("Hello World", "Arial", 40);
-                    label.setPosition(size.width / 2, size.height / 2);
-                    this.addChild(label, 1);
-                }
-            });
-            cc.director.runScene(new MyScene());
-        }, this);
-    };
-    cc.game.run("gameCanvas");
-
-})(jQuery, cc);
+})(jQuery);
--- socket/game.js
+++ socket/game.js
@@ -5,8 +5,14 @@ module.exports = function(io){
     .on('connection', function(socket){
         //监听新用户加入
         socket.on('login', function(obj, cb){
+            //判断是否是匿名用户
+            if(!obj.id){
+                //未登录用户id等于socketid
+                obj.id = socket.id;
+                obj.name = '匿名';
+            }
             //将新加入用户的唯一标示当做socket的名称， 后面退出的时候会用到
-            socket.id = obj.id;
+            socket.name = obj.id;
             //检查在线列表，如果不在里面就加入
             if(!onlineUsers.hasOwnProperty(obj.id)){
                 onlineUsers[obj.id] = obj.name;
@@ -16,29 +22,45 @@ module.exports = function(io){
             //登录成功之后执行cb
             cb && cb();
             //通知客户端更新信息
-            socket.broadcast.emit('update', {
-                onlineCount: onlineCount
+            game.emit('update', {
+                onlineCount: onlineCount,
+                onlineUsers: onlineUsers
             });
             console.log(obj.name + '加入了聊天室');
             
         });
-        
-    })
-    //监听用户退出
-    .on('disconnect', function(socket){
-        //将退出用户从在线列表中删除
-        if(onlineUsers.hasOwnProperty(socket.id)){
-            //退出用户的信息
-            var obj = { id: socket.id, name: onlineUsers[socket.id] };
-            //删除
-            delete onlineUsers[socket.id];
-            //在线人数-1
-            onlineCount--;
-            //向所有客户端广播用户退出
-            socket.emit('update', {
-                onlineCount: onlineCount
+        //监听用户退出
+        socket.on('disconnect', function(){
+            //将退出用户从在线列表中删除
+            console.log(socket.name);
+            if(onlineUsers.hasOwnProperty(socket.name)){
+                //退出用户的信息
+                var obj = { id: socket.name, name: onlineUsers[socket.name] };
+                //删除
+                delete onlineUsers[socket.name];
+                //在线人数-1
+                onlineCount--;
+                //向所有客户端广播用户退出
+                game.emit('update', {
+                    onlineCount: onlineCount,
+                    onlineUsers: onlineUsers
+                });
+                console.log(obj.name + '退出了聊天室');
+            }
+        });
+
+        //监听用户发布聊天内容
+        socket.on('message', function(obj, cb){
+            //向所有客户端广播发布的消息
+            var user = { id: socket.name, name: onlineUsers[socket.name], message: obj.message };
+            console.log(obj);
+            game.emit('message', user);
+            cb && cb({
+                message: '发送成功',
+                status: 200
             });
-            console.log(obj.name + '退出了聊天室');
-        }
+            console.log(user.name + ':' + user.message);
+        });
+
     });
-}
+};
